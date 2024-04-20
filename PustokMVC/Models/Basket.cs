6a1b08346d73d03e6f0668740827594d63fdc917
HFarhad2843 <146637915+HFarhad2843@users.noreply.github.com>
2024-04-20 15:29:43 +0400

    PustokMVC

---
+++ PustokMVC/Models/Basket.cs
@@ -0,0 +1,12 @@
+﻿namespace PustokMVC.Models
+{
+    public class Basket
+    {
+        public string UserId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public Product Product { get; set; }
+        public AppUser AppUser { get; set; }
+
+    }
+}
---
+++ PustokMVC/ViewModels/BasketItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PustokMVC.Models;
+
+namespace PustokMVC.ViewModels
+{
+    public class BasketItemViewModel
+    {
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public int Count { get; set; }
+    }
+}
